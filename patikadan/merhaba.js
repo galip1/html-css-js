1ce341f939a2f1337b71e969e20d0fa169318e9a
galip <galipts1@gmail.com>
2022-11-27 15:35:49 +0300

    v1

---
+++ patikadan/merhaba.js
@@ -0,0 +1,122 @@
+//console.log("merhaba");
+//alert("merhaba");
+//document.write("merhaba dünya");
+
+/*var serverName = "welcome";
+console.log(serverName);
+serverName = "www.google.com";
+console.log(serverName);
+document.write("www.google.com");
+
+let fullName = "ahmetcan";
+console.log(fullName);
+
+fullName += " " + "ve " + "alican";
+console.log(fullName);
+
+const SERVER_PASWORD = 123;
+console.log(SERVER_PASWORD);*/
+
+/*let isActive = true;
+let hasPassword = false;
+
+console.log(isActive);*/
+
+/*let nme = "ali";
+let surName = "veli";
+console.log(nme, surName);*/
+
+/*let isActive = false;
+
+isActive = true;
+console.log(isActive);
+
+let userName = 0.1;
+let isUserName = Boolean(userName);
+
+console.log(isUserName);*/
+
+/*let price = 110;
+let sprintPrice = "110";
+let hasPassword = true;
+
+console.log(typeof price, typeof sprintPrice, typeof hasPassword);
+
+price = price.toString();
+sprintPrice = Number("110");
+hasPassword = hasPassword.toString();
+console.log(typeof price, typeof sprintPrice, typeof hasPassword);
+
+let number = "11.5";
+number = parseFloat(number);
+
+console.log(number, typeof number);
+*/
+
+/*let userName = "hakan";
+const DOMAIN = "kodluyoruz.org";
+
+let email = userName + "@" + DOMAIN;
+
+console.log(
+  "merhaba " + userName + "sitemize hosgeldin. Email adresin " + email
+);
+
+let info = `Merhaba username  ${userName} 
+sitemize hoşgeldin. email adresin ${email}
+ email adresi uzunlugu ${email.length}
+ borcunuz: ${2 ** 2 + 5} gunu saat bilgisi: 
+ ${new Date().getHours()}
+ 
+ ${userName[0]}`;
+
+console.log(info);*/
+
+/*let email = "hakan@gmail.com";
+let firstName = "hakan";
+let lastName = "yalcinkaya";
+let DOMAIN = email.slice(email.search("@") + 1);
+
+console.log(
+  email.length,
+  email[0],
+  firstName.toUpperCase(),
+  lastName.toLowerCase(),
+  lastName.toLocaleUpperCase(),
+  email.codePointAt(),
+  lastName.concat(),
+  lastName.lastIndexOf(),
+  lastName.repeat("y"),
+  lastName.includes("y"),
+  lastName.indexOf("a"),
+  lastName.search("a"),
+  lastName.slice(1, 5),
+  lastName.endsWith([2]),
+  DOMAIN.slice(0, DOMAIN.indexOf(".")),
+  email.replace("gmail", "hotmail"),
+  lastName.endsWith("ya")
+);*/
+
+/*console.log(2 ** 4);
+console.log(Math.floor(1.9));
+console.log(Math.ceil(1.9));
+console.log(Math.round(1.5));*/
+
+//let title = document.getElementsByTagName("h2");
+/*let title = document.getElementById("title");
+console.log((title.innerHTML = "deneme"));
+
+let link = document.querySelector("#kodluyoruzlink");
+link.innerHTML += " değişti";
+link.style.color = "red";
+link.classList.add("btn");
+let parag = document.querySelector("#lorem");
+parag.style.color = "blue";
+parag.classList.add("btn");
+parag.innerHTML += " Lorem10";*/
+
+/*let userName = prompt("adınız");
+
+let greeting = document.querySelector("#greeting");
+greeting.innerHTML = `${greeting.innerHTML} <small style="color:red"f>${userName}</small>`;
+*/
