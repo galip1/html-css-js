e1430d6ce876e30900ef2c66a2e79da18e6544ab
galip <galipts1@gmail.com>
2023-01-04 08:21:41 +0300

    js

--- patikadan/nasılsın.js
+++ patikadan/nasılsın.js
@@ -1,24 +1,2 @@
-/*let item = document.querySelector("ul#list>li:last-child");
-
-let lastChild = document.querySelector("ul#list>li:last-child");
-lastChild.innerHTML = "son öge deişti";
-
-let firstChild = document.querySelector("ul#list>li:first-child");
-firstChild.innerHTML = "ilk öge deişti";
-
-let ulDOM = document.querySelector("ul#list");
-let liDOM = document.createElement("li");
-
-/*liDOM.innerHTML = "yeni bir liste";
-ulDOM.innerHTML = "new a list";
-ulDOM.insertBefore(liDOM);
-liDOM.innerHTML = "list a new";
-ulDOM.prepend(liDOM);M.append(liDOM);
-liDO
-*/
-
-let dosya = document.querySelector("#ad");
-console.log(dosya.innerHTML);
-
-let head = document.querySelector("#greeting");
-console.log(head.innerHTML);
+const font = () => {};
+const name = () => {};
